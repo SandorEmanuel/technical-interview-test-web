a4e776b13dc952015964c458644aa80d4b6bbb51
SandorEmanuel <sandor_emi@yahoo.com>
2019-06-10 22:43:57 +0300

    Pre-exam

--- js/technical-interview-test.js
+++ js/technical-interview-test.js
@@ -15,8 +15,8 @@ window.Test = {
 
 
     addQuestionGivenAnswer: function (id,givenAnswer) {
+        console.log(id,givenAnswer);
 
-        var givenAnswer = $("input[title='answer']").value;
         var data = {
             'id': id,
             'givenAnswer': givenAnswer
@@ -28,19 +28,29 @@ window.Test = {
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify(data)
         }).done(function (response) {
-            console.log(response);
+            console.log("getting response:", response, typeof response);
+            response = JSON.parse(response);
 
+            //removes all previous color classes
+            // for (i = 1; i <= 3; i++) {
+            //     $("#add_given_answer_" + response.id + "_" + i).removeClass("right_answer");
+            //     $("#add_given_answer_" + response.id + "_" + i).removeClass("wrong_answer");
+            // }
+
+            $("#add_given_answer_" + response.id + "_" + response.answer).addClass(response.success === true ? "right_answer" : "wrong_answer");
+            for (i = 1; i <= 3; i++) {
+                $("#add_given_answer_" + response.id + "_" + i).attr("disabled", "disabled");
+            }
         });
     },
-
 
     getQuestionRow: function(question) {
         return `<tr>
     <td class="contentTitle">${question.contentTitle}
       <ul>
-        <li class="choice1"><input class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice1" value="1">${question.choice1}</li>
-        <li class="choice2"><input class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice2" value="2">${question.choice2}</li>
-        <li class="choice3"><input class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice3" value="3">${question.choice3}</li>
+        <li class="choice1"><button id="add_given_answer_${question.id}_1" class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice1" value="1">${question.choice1}</button></li>
+        <li class="choice2"><button id="add_given_answer_${question.id}_2" class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice2" value="2">${question.choice2}</button></li>
+        <li class="choice3"><button id="add_given_answer_${question.id}_3" class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice3" value="3">${question.choice3}</button></li>
       </ul>
     </td>  
     </tr>`
@@ -53,17 +63,45 @@ window.Test = {
 
         questions.forEach(question => rows += Test.getQuestionRow(question));
 
-        console.log(rows);
+        // console.log(rows);
 
         $('#questions tbody').html(rows)
     },
 
     bindEvents: function(){
-        $('#questions tbody').delegate('add_given_answer', 'click', function () {
+        $('#questions tbody').delegate('.add_given_answer', 'click', function () {
             var id = $(this).data('question_id');
-            var givenAnswer = $(this).value('value')
+            var givenAnswer = $(this).val();
 
             Test.addQuestionGivenAnswer(id,givenAnswer);
+        });
+    },
+
+    addUser: function(){
+        console.log('calling adduser');
+        var firstName = $("input[title='First Name']").val();
+        var lastName = $("input[title='Last Name']").val();
+        var email = $("input[title='Email']").val();
+
+        var data = {
+            'firstName': firstName,
+            'lastName' : lastName,
+            'email' : email
+        };
+
+        $.ajax({
+            url: Test.apiURL +"/results",
+            method: "POST",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(data)
+        }).done(function (response){
+            console.log(response);
+
+
+            $("#firstName").html($("#firstName").attr("data-prefix").replace("%%", response.firstName));
+            $("#lastName").html($("#lastName").attr("data-prefix").replace("%%", response.lastName));
+            $("#email").html($("#email").attr("data-prefix").replace("%%", response.email));
+            $("#result").html($("#result").attr("data-prefix").replace("%%", response.result));
         });
     }
 };
@@ -71,7 +109,6 @@ window.Test = {
 
 
 Test.getQuestions();
-
 Test.bindEvents();
 
 
