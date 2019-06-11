eeb4aa7031ad59aa487de45af68bbd91c2438ba7
SandorEmanuel <sandor_emi@yahoo.com>
2019-06-11 11:12:45 +0300

    modified for exam

--- js/technical-interview-test.js
+++ js/technical-interview-test.js
@@ -31,52 +31,12 @@ window.Test = {
             console.log("getting response:", response, typeof response);
             response = JSON.parse(response);
 
-            //removes all previous color classes
-            // for (i = 1; i <= 3; i++) {
-            //     $("#add_given_answer_" + response.id + "_" + i).removeClass("right_answer");
-            //     $("#add_given_answer_" + response.id + "_" + i).removeClass("wrong_answer");
-            // }
-
             $("#add_given_answer_" + response.id + "_" + response.answer).addClass(response.success === true ? "right_answer" : "wrong_answer");
-            for (i = 1; i <= 3; i++) {
+            for (i = 1; i <= 5; i++) {
                 $("#add_given_answer_" + response.id + "_" + i).attr("disabled", "disabled");
             }
         });
     },
-
-    getQuestionRow: function(question) {
-        return `<tr>
-    <td class="contentTitle">${question.contentTitle}
-      <ul>
-        <li class="choice1"><button id="add_given_answer_${question.id}_1" class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice1" value="1">${question.choice1}</button></li>
-        <li class="choice2"><button id="add_given_answer_${question.id}_2" class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice2" value="2">${question.choice2}</button></li>
-        <li class="choice3"><button id="add_given_answer_${question.id}_3" class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice3" value="3">${question.choice3}</button></li>
-      </ul>
-    </td>  
-    </tr>`
-    },
-
-    displayQuestions: function (questions){
-        console.log('Display questions.');
-
-        var rows = '';
-
-        questions.forEach(question => rows += Test.getQuestionRow(question));
-
-        // console.log(rows);
-
-        $('#questions tbody').html(rows)
-    },
-
-    bindEvents: function(){
-        $('#questions tbody').delegate('.add_given_answer', 'click', function () {
-            var id = $(this).data('question_id');
-            var givenAnswer = $(this).val();
-
-            Test.addQuestionGivenAnswer(id,givenAnswer);
-        });
-    },
-
     addUser: function(){
         console.log('calling adduser');
         var firstName = $("input[title='First Name']").val();
@@ -103,7 +63,42 @@ window.Test = {
             $("#email").html($("#email").attr("data-prefix").replace("%%", response.email));
             $("#result").html($("#result").attr("data-prefix").replace("%%", response.result));
         });
-    }
+    },
+
+    getQuestionRow: function(question) {
+        return `<tr>
+    <td class="contentTitle">${question.contentTitle}
+      <ul>
+        <li class="choice1"><button id="add_given_answer_${question.id}_1" class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice1" value="1">${question.choice1}</button></li>
+        <li class="choice2"><button id="add_given_answer_${question.id}_2" class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice2" value="2">${question.choice2}</button></li>
+        <li class="choice3"><button id="add_given_answer_${question.id}_3" class="add_given_answer" type="button" data-question_id="${question.id}" name ="${question.id}" title = "answer" id="Choice3" value="3">${question.choice3}</button></li>
+      </ul>
+    </td>  
+    </tr>`
+    },
+
+    displayQuestions: function (questions){
+        console.log('Display questions.');
+
+        var rows = '';
+
+        questions.forEach(question => rows += Test.getQuestionRow(question));
+
+
+
+        $('#questions tbody').html(rows)
+    },
+
+    bindEvents: function(){
+        $('#questions tbody').delegate('.add_given_answer', 'click', function () {
+            var id = $(this).data('question_id');
+            var givenAnswer = $(this).val();
+
+            Test.addQuestionGivenAnswer(id,givenAnswer);
+        });
+    },
+
+
 };
 
 
